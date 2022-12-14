217353958f505e7348323a77ef34e0ae9da08b14
mo7zayed <mohamedzayed907@yahoo.com>
2022-12-14 22:23:32 +0200

    updated down.sh

--- laravel/kubernetes/down.sh
+++ laravel/kubernetes/down.sh
@@ -1,2 +1,4 @@
 kubectl delete -f kubernetes/deployments/http.yml;
 kubectl delete -f kubernetes/services/http.yml;
+kubectl delete -f kubernetes/deployments/horizon.yml;
+kubectl delete -f kubernetes/services/horizon.yml;
